60d6e38ebc6122b6bc87d2c4a8048fb523cad4d1
sachin <nudhayaraj99@gmail.com>
2022-06-30 15:04:56 +0530

    Added yml file

--- src/main/java/com/eKart/serviceRegistry/ServiceRegistryEkartApplication.java
+++ src/main/java/com/eKart/serviceRegistry/ServiceRegistryEkartApplication.java
@@ -2,8 +2,10 @@ package com.eKart.serviceRegistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryEkartApplication {
 
 	public static void main(String[] args) {
